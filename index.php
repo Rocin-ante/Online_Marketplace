1156da02b4c35c73be441506eb8b3835e9f32513
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-02-28 22:32:16 +0100

    更新 index.php

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.cn/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.cn/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="res/bootstrap/css/bootstrap.min.css">
+    <script src="res/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>index</title>
 </head>
 <body>
     

91a36dc80102ccba0157ef9e2d27dc23a6cd5fce
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-03-18 19:45:42 +0100

    re

--- inc/navigation.php
+++ inc/navigation.php
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link fw-bold text-primary <?= ($site == "home") ? "active" : "" ?>" aria-current="page" href=".">Home</a>
+                <a class="nav-link fw-bold text-primary <?= ($site == "home") ? "active" : "" ?>" href="?site=home">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link fw-bold <?= ($site == "product") ? "active" : "" ?>" href="?site=product">Product</a>

13c53f60a3c95017ee2945f2bc380e192a74ab26
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-03-18 19:08:44 +0100

    re nav home

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand fw-bold" href="#">
+    <a class="nav-link <?= ($site == "home") ? "active" : "" ?>" aria-current="page" href=".">
         <img src="../Online_Marketplace/res/img/logo.png" alt="">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link fw-bold text-primary" aria-current="page" href="#">Home</a>
+                <a class="nav-link fw-bold text-primary <?= ($site == "home") ? "active" : "" ?>" aria-current="page" href=".">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link fw-bold text-primary <?= ($site == "product") ? "active" : "" ?>" href="?site=product">Product</a>

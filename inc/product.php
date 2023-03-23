62848a0e506cda1c44573eb8e3014d0d5b389b82
pchpchpch111 <126407744+pchpchpch111@users.noreply.github.com>
2023-03-24 00:36:32 +0100

    这是已完成的页面
    Hier ist die fertige Seite

---
+++ inc/product.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="../Online_Marketplace/res/bootstrap/css/home.css">
+<h2>Welcome!</h2>
+<body>
+<section>
+<h1>Do you like toys? Come and see!!</h1>
+<div class="main_img_part-home">
+<div class="part" style="width: 100%">
+<a href="">
+<center>
+<img src="../Online_Marketplace/res/img/wallhaven-q66pyq_1448x320.png" alt class="part-bg" ></a>
+</center>
+<h5 class="h5">
+</h5>
+</div>
+</div>
+<div style="display: flex;">
+<div class="col-md-4"><h1 class="word">Dinosaurier-Spielzeug 6€</h1>
+<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; left: 80%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
+<img class="article_img" src="../Online_Marketplace/res/img/toy1.jpg" alt>
+</div>
+<div class="col-md-4 "><h1 class="word">Bagger Spielzeug 7€</h1>
+<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; right: 80%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
+<img class="article_img" src="../Online_Marketplace/res/img/toy2.jpg" alt>
+</div>
+<div class="col-md-4"><h1 class="word">kleines Zugspielzeug 7€</h1>
+<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; left: 50%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
+<img class="article_img" src="../Online_Marketplace/res/img/toy3.jpg" alt>
+</div>
+</div>
+</body>

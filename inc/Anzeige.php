5edc358d37fe39b703e323e808155de4a8937448
XIE12334 <xieyangzheng123@gmail.com>
2023-05-01 22:53:44 +0200

    FAQ

---
+++ inc/Anzeige.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HelfsSeite</title>
+    <link rel="stylesheet" href="../res/css/bootstrap-5.2.2-dist/css/bootstrap.min.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet">
+
+
+    <style>
+    #title{   
+    font-family: 'Monoton', cursive;
+    }
+    </style>
+
+</head>
+<body class="bg-dark">
+
+<div class="container ">
+        <div class="row">
+                <div class="col-md-7">
+                    <img src="./res/img/FAQ.jpg" class="img-fluid"  height="auto" alt="Frage">
+                </div>
+                <div class="col-md-5 " id="title">
+                    <h1 class="text-warning text-center mt-4">
+                    FAQ
+                    </h1>
+                    <h2 class="text-warning text-center">
+                    <strong>
+                    Willkommen in unserem Online-Shop, wo alles, was Sie interessiert, 
+                    das Leben besteht aus Badürfnissen, wir erfüllen Ihre Bedürfnisse.
+                    </strong>
+                    </h2>
+                </div>
+        </div>  
+</div>
+<div class="container">
+    <div class="text-warning text-center" id="Inhalt">
+    <h1 class="text-warning text-center">
+    Was sind Online Shop? 
+    </h1>
+    <h3>
+    Unser Online-Shopping ist seit vielen Jahren in Betrieb und wird von den Verbrauchern weithin gelobt,
+    wir setzen uns dafür ein, ein freundliches Einkaufsumfeld zu schaffen,und unser Slogan ist:"Alles für das Leben!".
+    </h3>
+    </div> 
+    <br>
+    <div class="text-warning text-center">
+    <h1 class="text-warning">
+    Warum sollte ich mich registrieren ?
+    </h1>
+    <h3>
+    Sie können viele Werbeaktionen genießen und viele freundliche Verbraucher und Händler treffen!
+    Es kann einen besseren Benutzerservice bieten und personalisiertes Einkaufen erstellen.
+    </h3>
+    </div>
+    <br>
+    <div class="text-warning text-center">
+    <h1 class="text-warning">
+    Schneller und Besser !!! 
+    </h1>
+    <h3>
+    Du kannst räumliche Distanz überwinden, da du unabhängig von einem bestimmten Standort überregional,
+    wenn nicht sogar international potenzielle Kunden ansprechen und handeln kannst. 
+    Außerdem kannst du rund um die Uhr automatisiert handeln.
+    </h3>
+    </div>
+    <br>
+    <div class="text-warning text-center">
+    <h1 class="text-warning">
+    Unterschiedliche Bedürfnisse erfordern eine große Auswahl. 
+    </h1>
+    <h3>
+    Mit dem Online-Handel profitieren Konsumenten von einem riesigen Angebot, 
+    welches lokal nicht beschränkt ist. Das bietet ihnen auch bessere Vergleichsmöglichkeiten, 
+    um das für sich beste Produkt zu finden.
+    </h3>
+    </div>
+    <br>
+    <div class="text-warning text-center">
+    <h1 class="text-warning">
+    Ein reibungsloses Erlebnis beginnt mit dem Einkaufen!
+    </h1>
+    <h3>
+    Online-Shop ist für Konsumenten sehr bequem, da diese sich online umschauen,
+    einkaufen und bezahlen können. Die Ware wird anschließend bequem per Versanddienstleister zugestellt, 
+    wobei Produkte aus verschiedenen Gründen auch retourniert werden können. 
+    Das alles ist für Konsumenten innerhalb von kürzester Zeit möglich.
+    </h3>
+    </div>
+    <br>
+    <div class="text-warning text-center">
+    <h1 class="text-warning">
+    Weniger Prozesse, mehr gute Erfahrung
+    </h1>
+    <h3>
+    Kunden haben die Möglichkeit, einen direkten Kontakt zum Online-Händler herzustellen. 
+    Bei Problemen oder grundsätzlichen Fragen können sie auf eine schnelle Reaktionszeit setzen, 
+    ohne erneute Wege in ein Ladengeschäft vornehmen zu müssen.
+    </h3>
+    </div>
+</div>
+
+</body>
+</html>

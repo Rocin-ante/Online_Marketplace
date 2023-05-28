33fed232c79163a1d8cb7ea393893f087d08b042
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-05-28 11:12:10 +0200

    更新 product.php

--- inc/product.php
+++ inc/product.php
@@ -1,33 +1,3 @@
-<!--
-<link rel="stylesheet" href="../Online_Marketplace/res/css/product.css">
-<h2>Welcome!</h2>
-<body>
-<section>
-<h1>Do you like toys? Come and see!!</h1>
-<div class="main_img_part-home">
-<div class="part" style="width: 100%">
-<a href="">
-<img src="../Online_Marketplace/res/img/toy4.png" alt class="part-bg" ></a>
-<h5 class="h5">
-</h5>
-</div>
-</div>
-<div style="display: flex;">
-<div class="col-md-4"><h1 class="word">Dinosaurier-Spielzeug 6€</h1>
-<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; left: 80%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
-<img class="article_img" src="../Online_Marketplace/res/img/toy1.jpg" alt>
-</div>
-<div class="col-md-4 "><h1 class="word">Bagger Spielzeug 7€</h1>
-<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; right: 80%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
-<img class="article_img" src="../Online_Marketplace/res/img/toy2.jpg" alt>
-</div>
-<div class="col-md-4"><h1 class="word">kleines Zugspielzeug 7€</h1>
-<a href="add-to-cart.php?product_id=123" style="position: absolute; top: 70%; left: 50%; transform: translate(-50%, -50%); background-color: #f00; color: #fff; padding: 10px;">Add to Cart</a>
-<img class="article_img" src="../Online_Marketplace/res/img/toy3.jpg" alt>
-</div>
-</div>
-</body>
--->
 <div>
   <select id="category-select">
     <option value="all">All Categories</option>

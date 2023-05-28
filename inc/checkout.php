7464f5fdf6dbf6f35c9179c9f92458e0ed03c20c
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-05-28 14:51:54 +0200

    更新 checkout.php

--- inc/checkout.php
+++ inc/checkout.php
@@ -1,52 +1,61 @@
 <link rel="stylesheet" href="../ONLINE_MARKETPLACE/res/css/checkout.css">
 
 <div class="container">
-    <h1>Shopping Cart</h1>
+    <h1 class="mb-4">Shopping Cart</h1>
     
     <ul id="cart-items"></ul>
     
-    <div id="cart-total"></div>
+    <div class="mb-4" id="cart-total"></div>
     
-    <div id="checkout-form" style="display: none;">
-      <h2>Checkout</h2>
-      <form>
-          <label for="name">Name:</label>
-  <input type="text" id="name" required><br>
-  <label for="lastname">Last name:</label>
-  <input type="text" id="lastname" required><br>
-  <label for="address">Address:</label>
-  <input type="text" id="address" required><br>
-  <label for="addaddress">Additional adress:</label>
-  <input type="text" id="addaddress" required><br>
-  <label for="phone">Phone:</label>
-  <input type="tel" id="phone" required><br>
-  <button onclick="confirmOrder()">Confirm Order</button>
-      </form>
+    <div class="checkout-form-container" style="display: none;">
+        <h2 class="mb-4">Checkout</h2>
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name:</label>
+                <input type="text" id="name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Last name:</label>
+                <input type="text" id="lastname" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address:</label>
+                <input type="text" id="address" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="addaddress" class="form-label">Additional address:</label>
+                <input type="text" id="addaddress" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone:</label>
+                <input type="tel" id="phone" class="form-control" required>
+            </div>
+            <button onclick="confirmOrder()" class="btn btn-primary">Confirm Order</button>
+        </form>
     </div>
-       <h2>You may also like</h2>
-    <ul id="product-list">
-      <li>
-        <span>Dinosaurier-Spielzeug</span>
-        <span>$6</span>
-        <button class="add-to-cart-btn">Add to Cart</button>
-      </li>
-      <li>
-        <span>Bagger Spielzeug</span>
-        <span>$7</span>
-        <button class="add-to-cart-btn">Add to Cart</button>
-      </li>
-      <li>
-        <span>kleines Zugspielzeug</span>
-        <span>$6</span>
-        <button class="add-to-cart-btn">Add to Cart</button>
-      </li>
+    
+    <h2 class="mb-4">You may also like</h2>
+    <ul id="product-list" class="mb-4">
+        <li>
+            <span>Dinosaurier-Spielzeug</span>
+            <span>$6</span>
+            <button class="add-to-cart-btn btn btn-primary">Add to Cart</button>
+        </li>
+        <li>
+            <span>Bagger Spielzeug</span>
+            <span>$7</span>
+            <button class="add-to-cart-btn btn btn-primary">Add to Cart</button>
+        </li>
+        <li>
+            <span>kleines Zugspielzeug</span>
+            <span>$6</span>
+            <button class="add-to-cart-btn btn btn-primary">Add to Cart</button>
+        </li>
     </ul>
-    <a href="#" onclick="checkout()" class="checkout-button">Checkout</a>
-</div>
-
-<div id="checkout-form" style="display: none;">
-  <h2>Checkout</h2>
+    
+    <a href="#" onclick="checkout()" class="checkout-button btn btn-primary">Checkout</a>
 </div>
 
 <script src="res/bootstrap/js/jquery-3.6.4.min.js"></script>
-<script src="res/js/checkout.js" type="text/javascript"></script>  
+<script src="res/js/checkout.js" type="text/javascript"></script>
+

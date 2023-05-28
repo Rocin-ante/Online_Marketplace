16c6b6d6688cf64113ad3624ed7030144d3bf448
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-05-28 15:24:04 +0200

    创建 error.php

---
+++ inc/error.php
@@ -0,0 +1,93 @@
+
+    <style>
+        .main {
+            margin-top: 20px;
+            padding: 30px 30px;
+            border: 1px solid rgba(255, 255, 255, .3);
+            border-radius: 5px;
+        }
+
+        .main h2 {
+            font-size: 80px;
+            color: #000;
+        }
+
+        .main h1 {
+            font-size: 170px;
+            color: #000;
+        }
+
+        .main h3 {
+            margin: 40px 0px;
+            font-size: 28px;
+            color: #000;
+        }
+
+        @media screen and (max-width: 1000px) {
+            .main {
+                margin-left: 40px;
+            }
+
+            .main h2 {
+                font-size: 50px;
+            }
+
+            .main h1 {
+                font-size: 140px;
+            }
+
+            .main h3 {
+                margin: 20px 0px;
+                font-size: 20px;
+            }
+        }
+
+        @media screen and (max-width: 700px) {
+            .main {
+                margin-left: 30px;
+            }
+
+            .main h2 {
+                font-size: 40px;
+            }
+
+            .main h1 {
+                font-size: 100px;
+            }
+
+            .main h3 {
+                margin: 20px 0px;
+                font-size: 15px;
+            }
+        }
+
+        @media screen and (max-width: 500px) {
+            .main {
+                margin-left: 20px;
+            }
+
+            .main h2 {
+                font-size: 30px;
+            }
+
+            .main h1 {
+                font-size: 80px;
+            }
+
+            .main h3 {
+                margin: 20px 0px;
+                font-size: 12px;
+            }
+        }
+    </style>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 main">
+                <h2>OOPS</h2><br>
+                <h1>ERROR</h1><br>
+                <h1>404</h1><br>
+                <h3>"Sorry! The page you are looking for can't be found"</h3><br><br>
+            </div>
+        </div>
+    </div>

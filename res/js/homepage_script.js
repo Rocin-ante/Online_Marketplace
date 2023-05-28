de8c41e5ec6abbf61b832755e071455021539a2f
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-05-28 13:54:31 +0200

    home page

--- res/js/homepage_script.js
+++ res/js/homepage_script.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
+    loadProducts();
     loaddata();
 });
+
+function loadProducts() {
+    $.ajax({
+        type: "GET",
+        url: "../../../Online_Marketplace/backend/serviceHandler.php",
+        cache: false,
+        data: { method: "queryProduct", param: 0 },
+        dataType: "json",
+        success: function(response) {
+        // Clear the existing product list
+        $("#product-list").empty();
+
+            // Generate the product cards dynamically
+            for (var i = 0; i < 3; i++) {
+                var product = response[i];
+                var productHtml = `
+                    <div class="col-12 col-md-4">
+                        <div class="card mb-4 shadow rounded">
+                            <img src="${product.product_image}" class="card-img-top img-fluid" alt="${product.product_name}">
+                            <div class="card-body">
+                                <h5 class="card-title text-center"><b>${product.product_name}</b></h5>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                $("#product-list").append(productHtml);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error(error);
+        }
+    });
+}
 
 function loaddata() {
     $.ajax({
@@ -21,3 +55,4 @@ function loaddata() {
         }
     })
 }
+

e310cae915176220573f55423920ce1e95f140c8
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-06-08 16:03:53 +0200

    更新 search.php

--- inc/search.php
+++ inc/search.php
@@ -6,3 +6,4 @@
 
 <script src="res/bootstrap/js/jquery-3.6.4.min.js"></script>
 <script src="res/js/search_script.js" type="text/javascript"></script>  
+<script src="res/js/purchase_script.js" type="text/javascript"></script>

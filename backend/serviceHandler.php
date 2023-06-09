8a5697f6f516c128160d7c43d7e8623a71ca8876
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-06-09 12:35:30 +0200

    更新 serviceHandler.php

--- backend/serviceHandler.php
+++ backend/serviceHandler.php
@@ -4,21 +4,31 @@ include("businesslogic/simpleLogic.php");
 $param = "";
 $method = "";
 
-isset($_GET["method"]) ? $method = $_GET["method"] : false;
-isset($_GET["param"]) ? $param = $_GET["param"] : false;
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    isset($_GET["method"]) ? $method = $_GET["method"] : false;
+    isset($_GET["param"]) ? $param = $_GET["param"] : false;
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    isset($_POST["method"]) ? $method = $_POST["method"] : false;
+    isset($_POST["param"]) ? $param = $_POST["param"] : false;
+} else {
+    // 不支持的请求方法
+    response("UNKNOWN", 405, "Method not supported yet!");
+    exit();
+}
 
 $logic = new SimpleLogic();
 $result = $logic->handleRequest($method, $param);
 if ($result === null) {
     $result = []; // 返回空数组作为结果
 }
-response("GET", 200, $result);
+response($_SERVER['REQUEST_METHOD'], 200, $result);
 
 function response($method, $httpStatus, $data)
 {
     header('Content-Type: application/json');
     switch ($method) {
         case "GET":
+        case "POST":
             http_response_code($httpStatus);
             echo (json_encode($data));
             break;
@@ -27,4 +37,3 @@ function response($method, $httpStatus, $data)
             echo ("Method not supported yet!");
     }
 }
-

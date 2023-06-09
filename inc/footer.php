a74f2c62fe56085b7b038d27ede6ab3f6f5d23d4
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-06-09 14:20:48 +0200

    更新 footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
-<footer class="container-fluid bg-dark text-light py-3">
+<footer class="container-fluid bg-dark text-light py-0.5 fixed-bottom">
     <div class="container text-center">
+        <br>
         <p>All rights reserved &copy; 2023 Online_Market</p>
         <p ><a class="text-warning" nav-link <?= ($site == "Impressum") ? "active" : "" ?> href="?site=Impressum"> Impressum </a></p>
     </div>

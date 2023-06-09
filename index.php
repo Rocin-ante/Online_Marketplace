ee3255f5bf0b0de8d20822aa13daa458bc25c653
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-06-09 14:38:56 +0200

    更新 index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ session_start();
     <style>
         body    {
 	        font-family: "microsoft yahei";
-            margin-top: 100px; /* 与导航栏高度相等的 margin-top */
+            margin-top: 60px; /* 与导航栏高度相等的 margin-top */
             margin-bottom: 100px; /* 与页脚高度相等的 margin-bottom */
         }
     </style>

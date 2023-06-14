4f7e9cb97ba45907ff31d9b30e32781d8f6c3556
Rocin-ante <126396926+Rocin-ante@users.noreply.github.com>
2023-06-14 11:30:34 +0200

    更新 navigation.php

--- inc/navigation.php
+++ inc/navigation.php
@@ -85,8 +85,10 @@
                             }
                         ?>
                     </li>
+                        <!--
                     <li><hr class="dropdown-divider"></li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        -->
                 </ul>
             </li>
             <li class="nav-item">
